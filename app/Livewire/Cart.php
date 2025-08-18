e620b888df978a2d08f8b072057e4bf0ddcbaa6c
haopm <haopm@sankei-bc.co.jp>
2025-08-18 21:38:23 +0700

    a

--- app/Livewire/Cart.php
+++ app/Livewire/Cart.php
@@ -100,15 +100,34 @@ class Cart extends Component
         try {
             $connector = new NetworkPrintConnector("192.168.1.235", 9100);
             $printer = new Printer($connector);
-
-            // Bỏ dấu tiếng Việt
             $removeAccents = function (?string $str): string {
                 if ($str === null || $str === '') {
                     return '';
                 }
-                $str = iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE', $str);
-                return preg_replace('/[^ -~]/', '', $str);
+                $unicode = [
+                    'a' => 'á|à|ả|ã|ạ|ă|ắ|ằ|ẳ|ẵ|ặ|â|ấ|ầ|ẩ|ẫ|ậ',
+                    'A' => 'Á|À|Ả|Ã|Ạ|Ă|Ắ|Ằ|Ẳ|Ẵ|Ặ|Â|Ấ|Ầ|Ẩ|Ẫ|Ậ',
+                    'd' => 'đ',
+                    'D' => 'Đ',
+                    'e' => 'é|è|ẻ|ẽ|ẹ|ê|ế|ề|ể|ễ|ệ',
+                    'E' => 'É|È|Ẻ|Ẽ|Ẹ|Ê|Ế|Ề|Ể|Ễ|Ệ',
+                    'i' => 'í|ì|ỉ|ĩ|ị',
+                    'I' => 'Í|Ì|Ỉ|Ĩ|Ị',
+                    'o' => 'ó|ò|ỏ|õ|ọ|ô|ố|ồ|ổ|ỗ|ộ|ơ|ớ|ờ|ở|ỡ|ợ',
+                    'O' => 'Ó|Ò|Ỏ|Õ|Ọ|Ô|Ố|Ồ|Ổ|Ỗ|Ộ|Ơ|Ớ|Ờ|Ở|Ỡ|Ợ',
+                    'u' => 'ú|ù|ủ|ũ|ụ|ư|ứ|ừ|ử|ữ|ự',
+                    'U' => 'Ú|Ù|Ủ|Ũ|Ụ|Ư|Ứ|Ừ|Ử|Ữ|Ự',
+                    'y' => 'ý|ỳ|ỷ|ỹ|ỵ',
+                    'Y' => 'Ý|Ỳ|Ỷ|Ỹ|Ỵ',
+                ];
+
+                foreach ($unicode as $nonAccent => $accent) {
+                    $str = preg_replace("/($accent)/u", $nonAccent, $str);
+                }
+
+                return $str;
             };
+
 
             // Format hàng bảng (cố định cột)
             $formatTableRow = function (array $cols): string {
